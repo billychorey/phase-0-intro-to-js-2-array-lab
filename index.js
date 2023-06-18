ad46d1f36682e018a472c66eedf305bcbfff3e8b
billy chorey <billychorey@gmail.com>
2023-06-18 15:28:16 -0400

    adds methods on arrays

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+  cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+  cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  const newArray = [...cats];
+  newArray.push(name);
+  return newArray;
+}
+
+function prependCat(name) {
+  const newArray = [...cats];
+  newArray.unshift(name);
+  return newArray;
+}
+
+function removeLastCat() {
+  const newArray = [...cats];
+  newArray.pop();
+  return newArray;
+}
+
+function removeFirstCat() {
+  const newArray = [...cats];
+  newArray.shift();
+  return newArray;
+}
