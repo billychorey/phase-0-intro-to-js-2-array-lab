5763b10818d90e6b0a8f7de45d0bd6915df6119f
Billy <billychorey@gmail.com>
2023-07-21 10:47:32 -0400

    giving this exercise some more time

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
-// Write your solution here!
+// // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"];
+
+// function destructivelyAppendCat() {
+//   cats.push("Ralph");
+// }
+
+// function destructivelyPrependCat() {
+//   cats.unshift("Bob");
+// }
+
+// function destructivelyRemoveLastCat() {
+//   cats.splice(-1);
+// }
+
+// function destructivelyRemoveFirstCat() {
+//   cats.shift();
+// }
+
+// function appendCat(name) {
+//   const newArray = [...cats];
+//   newArray.push(name);
+//   return newArray;
+// }
+
+// function prependCat(name) {
+//   const newArray = [...cats];
+//   newArray.unshift(name);
+//   return newArray;
+// }
+
+// function removeLastCat() {
+//   const newArray = [...cats];
+//   newArray.pop();
+//   return newArray;
+// }
+
+// function removeFirstCat() {
+//   const newArray = [...cats];
+//   newArray.shift();
+//   return newArray;
+// }
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat() {
+function destructivelyAppendCat(name) {
   cats.push("Ralph");
 }
 
@@ -14,29 +55,29 @@ function destructivelyRemoveLastCat() {
 }
 
 function destructivelyRemoveFirstCat() {
-  cats.shift();
+  cats.splice(0, 1);
 }
 
 function appendCat(name) {
-  const newArray = [...cats];
-  newArray.push(name);
-  return newArray;
+  const newCatsArray = [...cats];
+  newCatsArray.push("Broom");
+  return newCatsArray;
 }
 
 function prependCat(name) {
-  const newArray = [...cats];
-  newArray.unshift(name);
-  return newArray;
+  const newPrependedArray = [...cats];
+  newPrependedArray.unshift("Arnold");
+  return newPrependedArray;
 }
 
 function removeLastCat() {
-  const newArray = [...cats];
-  newArray.pop();
-  return newArray;
+  const oneLessCat = [...cats];
+  oneLessCat.splice(-1);
+  return oneLessCat;
 }
 
 function removeFirstCat() {
   const newArray = [...cats];
-  newArray.shift();
+  newArray.splice(0, 1);
   return newArray;
 }
